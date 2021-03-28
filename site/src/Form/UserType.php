e9c7f05d900b389349851226b700f4fc648a6755
Vincent <vincent.commin@etu.univ-poitiers.fr>
2021-03-28 12:20:09 +0200

    Ajout de type pour les champs du formulaire

--- site/src/Form/UserType.php
+++ site/src/Form/UserType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +15,21 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('login')
-            ->add('password')
-            ->add('name')
-            ->add('firstname')
-            ->add('birthdate')
+            ->add('login',
+                TextType::class,
+                ['label' => 'identifiant'])
+            ->add('password',
+                PasswordType::class,
+                ['label' => 'mot de passe'])
+            ->add('name',
+                TextType::class,
+                ['label' => 'nom'])
+            ->add('firstname',
+                TextType::class,
+                ['label' => 'prénom'])
+            ->add('birthdate',
+                DateType::class,
+                ['label' => 'date de naissance'])
         ;
     }
 
