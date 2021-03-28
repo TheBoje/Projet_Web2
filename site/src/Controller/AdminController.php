b3699547b329324014d339e6371cdc3628e4652d
Louis Leenart <louis.leenar@gmail.com>
2021-03-28 12:26:20 +0200

    wip ajout produit admin

--- site/src/Controller/AdminController.php
+++ site/src/Controller/AdminController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Form\ProductType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -38,4 +41,26 @@ class AdminController extends AbstractController
     {
         return $this->render("vues/admin/editProducts.html.twig");
     }
+
+    /**
+     * @Route("/add/product", name = "addProduct")
+     */
+    public function addProductAction(Request $request) : Response
+    {
+        $em = $this->getDoctrine()->getManager();
+        $form = $this->createForm(ProductType::class);
+        $form->add('send', SubmitType::class, ['label'=>'add product']);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $em->flush();
+            $this->addFlash('info', 'Formulaire valide, ajout à la base de données');
+            return $this->redirectToRoute('admin_editProducts');
+        }
+
+        $args = array('form_add_product'=>$form->createView());
+        return $this->render('vues/admin/addProduct.html.twig', $args);
+    }
+
 }
